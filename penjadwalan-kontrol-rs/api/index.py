458f67957d2a205596b1eb81b17f92697779ba80
rbbnalvn-star <rbbnalvn@gmail.com>
2026-08-26 14:17:08 +0700

    Delete penjadwalan-kontrol-rs directory

--- penjadwalan-kontrol-rs/api/index.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-# Tambahkan direktori 'backend' ke sys.path agar import di main.py (seperti import models, schemas, dll) berjalan dengan benar
-sys.path.append(os.path.join(os.path.dirname(__file__), "..", "backend"))
-
-# Impor app FastAPI utama
-from main import app
